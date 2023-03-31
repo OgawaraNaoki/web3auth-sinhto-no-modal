45fe01ef0800fa8ffbdce83f2a32c07c4b9d5348
Ogawara Naoki <ogawara_naoki@retail-ai.jp>
2023-03-31 13:54:44 +0900

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { App } from "./App";
+import { Callback } from "./Callback";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/web3auth-sinhto-no-modal" element={<App />} />
+      <Route path="/web3auth-sinhto-no-modal/callback" element={<Callback />} />
+    </Routes>
+  </BrowserRouter>
 );
+
+// export const AppRouting = () => {
+//   <BrowserRouter>
+//     <Routes>
+//       <Route path="/" element={<App />} />
+//       <Route path="/callback" element={<Callback />} />
+//     </Routes>
+//   </BrowserRouter>
+// }
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
